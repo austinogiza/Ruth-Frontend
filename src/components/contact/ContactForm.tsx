47ecd197e3db91986dcd229153fd9ac7680e0962
austinogiza <austinogiza@gmail.com>
2021-12-02 22:42:08 +0100

    remove wrapper

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -3,7 +3,11 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const ContactForm: FC = () => {
-  return <Body></Body>;
+  return (
+    <Body>
+      <Wrapper></Wrapper>
+    </Body>
+  );
 };
 
 const Body = styled.div``;
