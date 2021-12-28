d9b8bbc26ed79af5d06536a8682e08a093b01cde
austinogiza <austinogiza@gmail.com>
2021-12-28 17:34:26 +0100

    add overlay

--- src/components/home/Overlay.tsx
+++ src/components/home/Overlay.tsx
@@ -17,8 +17,7 @@ const Overlay = () => {
 const Body = styled.div`
   overflow: hidden;
   position: absolute;
-  height: 100vh;
-
+  height: 100%;
   width: 100%;
   z-index: 40;
   left: 0;
@@ -26,7 +25,7 @@ const Body = styled.div`
 `
 const Top = styled.div`
   position: absolute;
-  height: 100vh;
+  height: 100%;
 
   width: 100%;
   z-index: 40;
